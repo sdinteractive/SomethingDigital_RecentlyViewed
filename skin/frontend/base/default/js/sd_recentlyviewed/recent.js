c04cec1ebb37b81af55e482f6f9b2ffa9ffd18bf
Phillip Jackson <philwinkle@gmail.com>
2015-04-27 14:12:27 -0400

    Localstorage with store config limit and dedupe

--- skin/frontend/base/default/js/sd_recentlyviewed/recent.js
+++ skin/frontend/base/default/js/sd_recentlyviewed/recent.js
@@ -1,8 +1,26 @@
 document.observe('dom:loaded',function(){
 	//get localstorage and append
-	var html = localStorage.getItem('recently-viewed') || '',
-		recent = document.getElementById('recently-viewed').innerHTML;
+	var recentlyViewed = JSON.parse(localStorage.getItem('recently-viewed')) || [];
 
-	console.log(html += recent);
-	localStorage.setItem('recently-viewed',html);
+	//get unique keys
+	var uniqueKeys = recentlyViewed.reduce(function(memo,a){
+		if(memo.indexOf(a.id) === -1){
+			memo.push(a.id);
+		}
+		return memo;
+	}, []);
+
+	//add if current product is not in the unique keys
+	if(uniqueKeys.indexOf(sdRecentlyViewed.productId) === -1){
+		recentlyViewed.push({
+			id: sdRecentlyViewed.productId,
+			html: document.getElementById('recently-viewed').innerHTML
+		});
+
+		if(recentlyViewed.length > sdRecentlyViewed.maxDisplay){
+			recentlyViewed.shift();
+		}
+	}
+
+	localStorage.setItem('recently-viewed',JSON.stringify(recentlyViewed));
 });
