1d502b0e76e8e8884e8e1f9c664c66f6e3fa448f
Phillip Jackson <philwinkle@gmail.com>
2015-04-28 11:42:06 -0400

    Refactor recent.js for a more OOP style approach

--- skin/frontend/base/default/js/sd_recentlyviewed/recent.js
+++ skin/frontend/base/default/js/sd_recentlyviewed/recent.js
@@ -1,26 +1,41 @@
-document.observe('dom:loaded',function(){
-	//get localstorage and append
-	var recentlyViewed = JSON.parse(localStorage.getItem('recently-viewed')) || [];
+var SomethingDigitalRecentlyViewed = Class.create();
 
-	//get unique keys
-	var uniqueKeys = recentlyViewed.reduce(function(memo,a){
-		if(memo.indexOf(a.id) === -1){
-			memo.push(a.id);
+SomethingDigitalRecentlyViewed.prototype = {
+	initialize: function(){
+		document.observe('dom:loaded',this.initLocalStorage);
+		this.render();
+	},
+
+	initLocalStorage: function(){
+		//get localstorage and append
+		var recentlyViewed = JSON.parse(localStorage.getItem('recently-viewed')) || [];
+
+		//get unique keys
+		var uniqueKeys = recentlyViewed.reduce(function(memo,a){
+			if(memo.indexOf(a.id) === -1){
+				memo.push(a.id);
+			}
+			return memo;
+		}, []);
+
+		//add if current product is not in the unique keys
+		if(uniqueKeys.indexOf(sdRecentlyViewed.productId) === -1){
+			recentlyViewed.push({
+				id: sdRecentlyViewed.productId,
+				html: document.getElementById('recently-viewed').innerHTML
+			});
+
+			if(recentlyViewed.length > sdRecentlyViewed.maxDisplay){
+				recentlyViewed.shift();
+			}
 		}
-		return memo;
-	}, []);
 
-	//add if current product is not in the unique keys
-	if(uniqueKeys.indexOf(sdRecentlyViewed.productId) === -1){
-		recentlyViewed.push({
-			id: sdRecentlyViewed.productId,
-			html: document.getElementById('recently-viewed').innerHTML
-		});
+		localStorage.setItem('recently-viewed',JSON.stringify(recentlyViewed));
+	},
 
-		if(recentlyViewed.length > sdRecentlyViewed.maxDisplay){
-			recentlyViewed.shift();
-		}
+	render: function(){
+		console.log('rendered');
 	}
+}
 
-	localStorage.setItem('recently-viewed',JSON.stringify(recentlyViewed));
-});
+new SomethingDigitalRecentlyViewed();
